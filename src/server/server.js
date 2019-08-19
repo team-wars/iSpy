29233a421a16fbb6fd5194eb95fba1589bcbc9f9
Will Robinson <wrobinson91@gmail.com>
2019-08-19 16:53:36 -0400

    hmr working, edited server file to serve up app.jsx

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-  res.send('hello world');
-})
+
+// app.use('/static', express.static(path.join(__dirname, '../build')));
+
+
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+// res.send('hello world');
 // /api/session/new
 // /api/session/join/:sessionid
 // /api/game/pick-team
@@ -13,4 +18,4 @@ app.get('/',(req,res)=>{
 // /api/game/clue
 // /api/game/pick-tile
 
-app.listen(PORT, ()=> console.log(`Started listening to port ${PORT}`));
+app.listen(PORT, () => console.log(`Started listening to port ${PORT}`));
