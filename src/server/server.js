6ecffec1c68b65868c458ca96ec166f981f2bf13
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-20 15:57:42 -0400

    api router

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-
+const apiRouter = require('./routers/apiRouter')
 const app = express();
-
 const PORT = 3000;
 
 
 // app.use('/static', express.static(path.join(__dirname, '../build')));
-
-
+app.use(express.json());
+app.use('/api', apiRouter);
 app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+
 // res.send('hello world');
 // /api/session/new
 // /api/session/join/:sessionid
