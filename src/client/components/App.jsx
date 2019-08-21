55d134237c4c37aed14d505344426c6264d6f0b3
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-21 15:51:49 -0400

    added ready toggle

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -18,13 +18,21 @@ const App = () => {
     socket.emit('join session', username);
     socket.on('joined', (msg) => console.log(msg));
   };
+  const readyToggle = () => {
+    const socketConnect = (room) => io('localhost:3000', {
+      query: `r_var=${room}`,
+    });
+    const socket = socketConnect(roomNum);
+    socket.emit('ready', username);
+    socket.on('ready changed', (msg) => console.log(msg));
+  };
 
   return (
     <>
       <input type="text" value={username} onChange={(e) => changeUser(e.target.value)} />
       <input type="text" value={roomNum} onChange={(e) => changeRoom(e.target.value)} />
       <button type="button" onClick={handleClick}>testing</button>
-
+      <button type="button" onClick={readyToggle}>Ready Up</button>
       <GameContainer />
       <ListContainer />
       <SpymasterContainer />
