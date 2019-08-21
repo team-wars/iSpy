254fce5eb93f3b49374f8ad64cf397ca417090cb
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-21 10:56:52 -0400

    implemented sockets on server for proof of concept
    Co-authored-by: Rocky Liao <rliao3613@gmail.com>

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,15 @@
-const express = require('express');
+// const express = require('express');
 const path = require('path');
-const apiRouter = require('./routers/apiRouter')
 const sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const apiRouter = require('./routers/apiRouter');
+
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 const PORT = 3000;
-
-
 // app.use(sassMiddleware({
 //   /* Options */
 //   src: path.join(__dirname, '../stylesheet'),
@@ -17,21 +21,17 @@ const PORT = 3000;
 // }), express.static(path.join(__dirname, 'build')));
 
 
-app.use('/static', express.static(path.join(__dirname, '../build')));
-// app.use('/static', express.static('build'));
-
-
-app.use('/static', express.static(path.join(__dirname, '../build')));
 app.use(express.json());
+app.use('/build', express.static(path.join(__dirname, '..', '..', 'build')));
 app.use('/api', apiRouter);
 app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
 
-// res.send('hello world');
-// /api/session/new
-// /api/session/join/:sessionid
-// /api/game/pick-team
-// /api/game/start
-// /api/game/clue
-// /api/game/pick-tile
-
-app.listen(PORT, () => console.log(`Started listening to port ${PORT}`));
+io.on('connection', (socket) => {
+  const room = socket.handshake.query.r_var;
+  socket.join(room);
+  socket.on('join session', (user) => {
+    console.log('recieved from client: ', user, room);
+    io.to(room).emit('joined', `${user} has joined room ${room}!`);
+  });
+});
+server.listen(PORT, () => console.log(`Started listening to port ${PORT}`));
