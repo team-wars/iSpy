b5872a66fc7a9ebd5dbfae5a9db4c7611c68492a
Will Robinson <wrobinson91@gmail.com>
2019-08-21 20:26:01 -0400

    added import for clue display

--- src/client/containers/GameContainer.jsx
+++ src/client/containers/GameContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Board from '../components/Board';
 import ChatboxContainer from './ChatboxContainer';
+import ClueDisplay from '../components/ClueDisplay';
 
 const mapStateToProps = (state) => ({
   sessionID: state.game.sessionID,
@@ -26,7 +27,7 @@ class GameContainer extends Component {
           {`Welcome to iSpy! Session: ${sessionID || 'TBD'}`}
         </h2>
         This is the Game Container
-        <ClueDisplay guesses={guessesLeft} clue={currentClue}/>
+        <ClueDisplay guesses={guessesLeft} clue={currentClue} />
         <Board />
         <ChatboxContainer />
       </section>
