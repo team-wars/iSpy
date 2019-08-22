581fef2b08c1e970587653e8e2acab4eb04a42e2
SeemsRocky <rliao3613@gmail.com>
2019-08-21 20:43:00 -0400

    not sure what this commit is

--- src/client/components/Chatbox.jsx
+++ src/client/components/Chatbox.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
-const Chatbox = props => (
-  <section>
-    
-
-  </section>
-);
+import { useSelector } from 'react-redux'
+const Chatbox = (props) => {
+  // let {messages} = useSelector((store)=>store.game)
+  handleKeyPress(){
+    // if enter, do the emitting shit
+    if (event.keyCode === 13) {
+      const userMsg = event.target.value;
+    }
+  };
+  const messageArr = [];
+  for(const msgObj of messages){
+    messageArr.push(<Message text={msgObj.text} user={msgObj.user}/>)
+  }
+  return (
+    <section>
+      <div id='messageContainer'>
+        {messageArr}
+      </div>
+      <input onKeyDown={handleKeyPress} />
+    </section>
+  );
+};
 
 export default Chatbox;
