18c5938a641f8365405075933e47311bb139ba75
SeemsRocky <rliao3613@gmail.com>
2019-08-22 11:54:57 -0400

    finished home route testing for puppeteer, limited testing on tile component

--- __tests__/EnzymeTests.js
+++ __tests__/EnzymeTests.js
@@ -4,6 +4,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import List from '../src/client/components/List';
+import Tile from '../src/client/components/Tile';
 
 configure({ adapter: new Adapter() });
 
@@ -11,5 +12,24 @@ describe('testing FormContainer Rendering', () => {
   it('should render a List Component!', () => {
     const wrapper = shallow(<List list={['ro', 'roc', 'rock']} color="blue" listType="team" />);
     expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe('testing Tile Component', () => {
+  let props;
+  beforeAll(() => {
+    props = {
+      testClick: jest.fn(),
+      word: 'rock',
+      affiliation: 'blue',
+      selectTile: jest.fn(),
+    };
+  });
+
+  it('should render a Tile component properly!', () => {
+    const wrapper = shallow(<Tile {...props} />);
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.text()).toBe('rock');
+    expect(wrapper.find('button').hasClass('unselected-tile-button')).toBe(true);
   });
 });
--- __tests__/PuppeteerTests.js
+++ __tests__/PuppeteerTests.js
@@ -30,7 +30,7 @@ describe('testing our life stories', () => {
       await page.waitForSelector('#roomID');
       await page.focus('#roomID');
       await page.keyboard.type('APPL');
-      const username = await page.$eval('#username', (el) => el.value);
+      const username = await page.$eval('#roomID', (el) => el.value);
       expect(username).toBe('APPL');
     });
 
@@ -42,10 +42,19 @@ describe('testing our life stories', () => {
       await page.waitForSelector('#roomID');
       await page.focus('#roomID');
       await page.keyboard.type('WARS');
-      const join = await page.waitForSelector('#Buttjoin');
-      await page.click(join);
+      await page.click('#Buttjoin');
       const route = await page.url();
       expect(route).toBe('http://localhost:3000/session/WARS');
+    });
+
+    it('can join game session after creating session', async () => {
+      await page.goto(APP);
+      await page.waitForSelector('#username');
+      await page.focus('#username');
+      await page.keyboard.type('Boulder');
+      await page.click('#Buttcreate');
+      const route = await page.url();
+      expect(route.indexOf('session')).toBeTruthy();
     });
   });
 });
