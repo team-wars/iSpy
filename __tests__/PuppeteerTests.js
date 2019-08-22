56a12002e013cf1f13685358c0d1f60275e74531
SeemsRocky <rliao3613@gmail.com>
2019-08-22 11:06:18 -0400

    added some more / route puppeteer implementation

--- __tests__/PuppeteerTests.js
+++ __tests__/PuppeteerTests.js
@@ -8,21 +8,44 @@ describe('testing our life stories', () => {
   let browser;
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu' ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu'],
     });
     page = await browser.newPage();
   });
   afterAll(() => {
     browser.close();
   });
-  describe('form from form?', () => {
-    it('can change username', async () => {
+  describe('Testing / route and all functionality', () => {
+    it('can input username', async () => {
       await page.goto(APP);
       await page.waitForSelector('#username');
       await page.focus('#username');
       await page.keyboard.type('Boulder');
       const username = await page.$eval('#username', (el) => el.value);
       expect(username).toBe('Boulder');
+    });
+
+    it('can input roomID', async () => {
+      await page.goto(APP);
+      await page.waitForSelector('#roomID');
+      await page.focus('#roomID');
+      await page.keyboard.type('APPL');
+      const username = await page.$eval('#username', (el) => el.value);
+      expect(username).toBe('APPL');
+    });
+
+    it('can join game session', async () => {
+      await page.goto(APP);
+      await page.waitForSelector('#username');
+      await page.focus('#username');
+      await page.keyboard.type('Boulder');
+      await page.waitForSelector('#roomID');
+      await page.focus('#roomID');
+      await page.keyboard.type('WARS');
+      const join = await page.waitForSelector('#Buttjoin');
+      await page.click(join);
+      const route = await page.url();
+      expect(route).toBe('http://localhost:3000/session/WARS');
     });
   });
 });
